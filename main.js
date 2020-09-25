58853627e489691d5b859c483b85c37251eaab63
Aaron Diggdon <aarondiggdon@gmail.com>
2020-09-24 17:22:21 -0700

    Added textbox styles that change dependant on the hour and refreshes every second

--- main.js
+++ main.js
@@ -9,45 +9,80 @@ date.textContent = moment().format('MMMM Do YYYY, h:mm:ss a');
 
 setInterval(function() {
 
-        
         date.textContent = moment().format('MMMM Do YYYY, h:mm:ss a');
         
-
   }, 1000);
   setInterval(event);
 
   setInterval(function() {
 
-    if (hour = 4){
+    console.log(hour);
+
+    if (hour == 4){
         document.getElementById("9text").classList.add('present');
         
     }
-    if (hour = 5){
+    if (hour == 5){
         document.getElementById("10text").classList.add('present');
         document.getElementById("9text").classList.add('past');
+        document.getElementById("11text").classList.add('future');
+        document.getElementById("12text").classList.add('future');
+        document.getElementById("1text").classList.add('future');
+        document.getElementById("2text").classList.add('future');
+        document.getElementById("3text").classList.add('future');
+        document.getElementById("4text").classList.add('future');
+        document.getElementById("5text").classList.add('future');
     }
-    if (hour = 6){
+    if (hour === 6){
         document.getElementById("11text").classList.add('present');
+        document.getElementById("10text").classList.add('past');
+        document.getElementById("12text").classList.add('future');
+        document.getElementById("1text").classList.add('future');
+        document.getElementById("2text").classList.add('future');
+        document.getElementById("3text").classList.add('future');
+        document.getElementById("4text").classList.add('future');
+        document.getElementById("5text").classList.add('future');
+        
     }
-    if (hour = 7){
+    if (hour == 7){
         document.getElementById("12text").classList.add('present');
+        document.getElementById("11text").classList.add('past');
+        document.getElementById("1text").classList.add('future');
+        document.getElementById("2text").classList.add('future');
+        document.getElementById("3text").classList.add('future');
+        document.getElementById("4text").classList.add('future');
+        document.getElementById("5text").classList.add('future');
     }
-    if (hour = 8){
+    if (hour == 8){
         document.getElementById("1text").classList.add('present');
+        document.getElementById("12text").classList.add('past');
+        document.getElementById("2text").classList.add('future');
+        document.getElementById("3text").classList.add('future');
+        document.getElementById("4text").classList.add('future');
+        document.getElementById("5text").classList.add('future');
     }
-    if (hour = 9){
+    if (hour == 9){
         document.getElementById("2text").classList.add('present');
+        document.getElementById("1text").classList.add('past');
+        document.getElementById("3text").classList.add('future');
+        document.getElementById("4text").classList.add('future');
+        document.getElementById("5text").classList.add('future');
     }
-    if (hour = 10){
+    if (hour == 10){
         document.getElementById("3text").classList.add('present');
+        document.getElementById("2text").classList.add('past');
+        document.getElementById("4text").classList.add('future');
+        document.getElementById("5text").classList.add('future');
     }
-    if (hour = 11){
+    if (hour == 11){
         document.getElementById("4text").classList.add('present');
+        document.getElementById("3text").classList.add('past');
+        document.getElementById("5text").classList.add('future');
     }
-    if (hour = 12){
+    if (hour == 12){
         document.getElementById("5text").classList.add('present');
+        document.getElementById("3text").classList.add('past'); 
     }
     
-
 }, 1000);
 
